b421de23ccee9a6219d38682d406afb20e2764f6
majinmilad <mchabok@calpoly.edu>
2024-12-15 23:43:13 -0800

    Delete rows added

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import MyApp from "./MyApp";  // imported with no suffix because Vite will accept .jsx, .js. .ts or .tsx files for transpiling
+import "./main.css";
+
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render:
+root.render(<MyApp />);

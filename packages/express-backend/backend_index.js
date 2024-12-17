cc0d5b4658ff22782cf28f128973e43dc65c6100
majinmilad <mchabok@calpoly.edu>
2024-12-16 21:38:17 -0800

    Implement getting users by id

--- packages/express-backend/backend_index.js
+++ packages/express-backend/backend_index.js
@@ -30,11 +30,26 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; // or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+// helpers
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 const users = {
   users_list: [

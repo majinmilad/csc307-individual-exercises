3ed23b17f1cce85c9a507022f1f5b8d95363b826
majinmilad <mchabok@calpoly.edu>
2024-12-16 20:54:07 -0800

    Implement first endpoint

--- packages/express-backend/backend_index.js
+++ packages/express-backend/backend_index.js
@@ -1,16 +1,54 @@
 import express from "express";
 
+// express API setup
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
+
+// API endpoints
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};

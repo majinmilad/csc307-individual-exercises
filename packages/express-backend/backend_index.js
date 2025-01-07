5588c5f25e01e7961628b0194c89b114a4ee7909
majinmilad <mchabok@calpoly.edu>
2025-01-06 22:15:54 -0800

    Remove old code

--- packages/express-backend/backend_index.js
+++ packages/express-backend/backend_index.js
@@ -23,27 +23,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// OLD
-// // gets users by name and/or job via query or gets all users
-// app.get("/users", (req, res) => {
-//   const name = req.query.name;
-//   const job = req.query.job;
-  
-//   let result = users; // if no name or job is specified it starts with with the whole users list and sends that   
-
-//   // filter by name and/or job if applicable
-//   if (name != undefined) {
-//     result = findUserByName(name, result); // gives filtered array based on name
-//     result = { 'users_list': result }; // wrap array into response dictionary form
-//   }
-//   if (job != undefined) {
-//     result = findUserByJob(job, result); // gives filtered array based on job
-//     result = { 'users_list': result }; // wrap array into response dictionary form
-//   }
-
-//   res.send(result); // sends response to API URL endpoint
-// });
-
 // gets users by name and/or job via query or gets all users
 app.get("/users", (req, res) => {
   const name = req.query.name; // extract 'name' query parameter
@@ -61,18 +40,6 @@ app.get("/users", (req, res) => {
       res.status(500).send({ error: "Internal Server Error" });
     });
 });
-
-// OLD
-// // gets a user by id
-// app.get("/users/:id", (req, res) => {
-//   const id = req.params["id"]; // or req.params.id
-//   let result = findUserById(id);
-//   if (result === undefined) {
-//     res.status(404).send("Resource not found.");
-//   } else {
-//     res.send(result);
-//   }
-// });
 
 // gets a user by id (doesn't work if id not in correct 24 digit hex format)
 app.get("/users/:id", (req, res) => {
@@ -96,15 +63,6 @@ app.get("/users/:id", (req, res) => {
     });
 });
 
-// OLD
-// // adds a user
-// app.post("/users", (req, res) => {
-//   const userToAdd = req.body;
-//   userToAdd["id"] = Math.floor(Math.random() * 100000).toString();
-//   addUser(userToAdd);
-//   res.status(201).send(userToAdd);
-// });
-
 // adds a user
 app.post("/users", (req, res) => {
   const userToAdd = req.body; // Extract the user object from request body
@@ -121,19 +79,6 @@ app.post("/users", (req, res) => {
     });
 });
 
-// OLD
-// // deletes first instance of a user by id
-// app.delete("/users/:id", (req, res) => {
-//   const id = req.params.id;
-//   const index = users["users_list"].findIndex((user) => user.id.toString() === id.toString());
-//   if (index > -1) {
-//     users["users_list"].splice(index, 1);
-//     res.status(204).send("User found and deleted successfully");
-//   } else {
-//     res.status(404).send("User not found");
-//   }
-// });
-
 // deletes first instance of a user by id
 app.delete('/users/:id', async (req, res) => {
   const userToDel = req.params.id;
@@ -143,57 +88,3 @@ app.delete('/users/:id', async (req, res) => {
   }
   res.status(404).end();
 });
-
-// helpers OLD
-
-// const findUserByName = (name, users_list) => {
-//   return users_list["users_list"].filter(
-//     (user) => user["name"] === name
-//   );
-// };
-
-// const findUserByJob = (job, users_list) => {
-//   return users_list["users_list"].filter(
-//     (user) => user["job"] === job
-//   );
-// };
-
-// const findUserById = (id) =>
-//   users["users_list"].find((user) => user["id"] === id);
-
-// const addUser = (user) => {
-//   users["users_list"].push(user);
-//   return user;
-// };
-
-// data
-
-// const users = {
-//   users_list: [
-//     {
-//       id: "xyz789",
-//       name: "Charlie",
-//       job: "Janitor"
-//     },
-//     {
-//       id: "abc123",
-//       name: "Mac",
-//       job: "Bouncer"
-//     },
-//     {
-//       id: "ppp222",
-//       name: "Mac",
-//       job: "Professor"
-//     },
-//     {
-//       id: "yat999",
-//       name: "Dee",
-//       job: "Aspring actress"
-//     },
-//     {
-//       id: "zap555",
-//       name: "Dennis",
-//       job: "Bartender"
-//     }
-//   ]
-// };
